a5cc7eea3db402a372782e8e463d25fabbec8f49
takeshi0311 <fabio.1993.2019@gmail.com>
2020-02-09 17:36:37 +0900

    Basic認証

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,10 +25,36 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/secrets.yml }
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
+# secrets.ymlは.gitignoreに追加されているため、そのままデプロイしても、本番環境のリポジトリには追加されないため、
+# 明示的に本番環境にアップロードするよう、記述を追加。
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
+
+#設定した環境変数をcapistranoでの自動デプロイで利用するために、明示的に環境変数を指定する
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["BASIC_AUTH_USER"],
+  AWS_SECRET_ACCESS_KEY: ENV["BASIC_AUTH_PASSWORD"]
+}
