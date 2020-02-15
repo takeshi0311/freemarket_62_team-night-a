3644ac0d69df79e42f167c8691a49beee511cfdd
Konsei0516 <masaya19960516@gmail.com>
2020-02-15 18:03:44 +0900

    imageモデル作成
    imageモデル作成
    reference :itemで外部キー制約

---
+++ db/migrate/20200215085711_create_images.rb
@@ -0,0 +1,10 @@
+class CreateImages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :images do |t|
+      t.string :image, null: false
+      t.reference :item, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
