49cee60814ed41ddd4279b83cf5e9e11bf99cc13
hepuburn <57085978+hepuburn@users.noreply.github.com>
2020-02-16 17:56:36 +0900

    usersテーjブル1つ削除しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200208063728) do
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 ActiveRecord::Schema.define(version: 20200215114634) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -81,18 +69,15 @@ ActiveRecord::Schema.define(version: 20200215114634) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname",                          null: false
-    t.string   "email",                             null: false
-    t.string   "password",                          null: false
-    t.string   "family_name",                       null: false
-    t.string   "last_name",                         null: false
-    t.string   "frigana_family_name",               null: false
-    t.string   "frigana_last_name",                 null: false
-    t.integer  "birthday",                          null: false
-    t.integer  "phone_number",                      null: false
-    t.text     "profile",             limit: 65535
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
