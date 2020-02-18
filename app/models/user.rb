f0e783269adbdfd214a632ddbecae0b79e0b8932
hepuburn <57085978+hepuburn@users.noreply.github.com>
2020-02-18 23:22:34 +0900

    アソシエーション追加userテーブル絡む追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+
+        has_many :items
+        has_many :purchases
+        has_many :comments
+        has_one :pay
+        has_one :address
 end
