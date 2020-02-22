99198835a588da08bda3a7c7c29417f324c0002c
kazu-west <jojo6251full@icloud.com>
2020-02-22 14:09:55 +0900

    routes.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'tweets#index'
-  get 'tweets' => 'tweets#index'
-  get 'users/loginid' => 'users#show'
-  get "users/logout" => "users#logout"
+  resources :users do
+    collection do
+      get 'logout'
+    end
+  end
+
 end
