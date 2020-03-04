9a473e462716e60cc125cd341d28eb5683978e4c
Konsei0516 <masaya19960516@gmail.com>
2020-03-04 21:38:08 +0900

    条件分岐
    クレジットカード表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,13 @@ class ItemsController < ApplicationController
   end
   
   def buy
+    @pay = Pay.where(user_id: current_user.id).first
+    if @pay.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(@pay.customer_id)
+      @default_card_information = customer.cards.retrieve(@pay.card_id)
+    end
   end
 end
