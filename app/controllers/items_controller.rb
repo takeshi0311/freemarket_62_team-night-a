ca9919f09153600417a463e28ce400141eb6eeb8
satoshi815 <57768913+satoshi815@users.noreply.github.com>
2020-03-09 16:04:53 +0900

    ユーザー毎の購入、編集ページ制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+
+  before_action :ensure_correct_user,{only: [:edit,:buy,]} 
+
   def index
     @image = Image.all
     @item = Item.all
@@ -8,22 +11,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    gon.image = Image.find(1)
-    @image = Image.find(1)
-    @item = Item.find(1)
+    gon.image = Image.find_by(id:1)
+    @image = Image.find_by(id:1)
+    @item = Item.find_by(id:1)
+    @user = User.find_by(id:1)
   end
 
+  # 出品者以外がURLから直接的に編集、購入画面に進めないようにするため
+  def ensure_correct_user
+      @item = Item.find_by(id:1)
+    if @item.user_id != current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/items/show")
+    end
+  end
 
   def edit
   end 
 
 end
-
-
-
-# def ensure_correct_user
-#   @post = Post.find_by(id:params[:id])
-#   if @user.user_id != @current_user.id
-#     flash[:notice] = "権限がありません"
-#     redirect_to("/items/edit")
-# end
