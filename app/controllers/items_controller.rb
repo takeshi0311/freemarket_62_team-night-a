1939f97a9d374768607a8bc9c76f83b1d85957ae
satoshi815 <57768913+satoshi815@users.noreply.github.com>
2020-03-09 17:44:45 +0900

    find、prefix変更しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,18 +11,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    gon.image = Image.find_by(id:1)
-    @image = Image.find_by(id:1)
-    @item = Item.find_by(id:1)
-    @user = User.find_by(id:1)
+    gon.image = Image.find(1)
+    @image = Image.find(1)
+    @item = Item.find(1)
+    @user = User.find(1)
   end
 
   # 出品者以外がURLから直接的に編集、購入画面に進めないようにするため
   def ensure_correct_user
-      @item = Item.find_by(id:1)
+      @item = Item.find(1)
     if @item.user_id != current_user.id
       flash[:notice] = "権限がありません"
-      redirect_to("/items/show")
+      redirect_to("item_path")
     end
   end
 
