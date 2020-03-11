8fe0ebaea87c6529c0b8991ab5e340154bc30b2b
satoshi815 <57768913+satoshi815@users.noreply.github.com>
2020-03-11 19:19:31 +0900

    修正しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,11 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.create(text: comment_params[:text], item_id: comment_params[:item_id], user_id: current_user.id)
     redirect_to "/items/#{comment.item.id}"   #コメントと結びつくアイテムの詳細画面に遷移す
+    if comment.save
+    else
+      flash[:notice] = "コメントできませんでした"
+    end
   end
-    
-  @comment = Comment.create(text: comment_params[:text], item_id: comment_params[:item_id], user_id: current_user.id)
-
-  if @comment.save
-  else
-    flash[:notice] = "コメントできません"
-  end
-   
-
 
   private
   def comment_params
