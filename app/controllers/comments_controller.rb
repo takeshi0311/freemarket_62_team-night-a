7d1e1b0920b8835b190ca98897eaa855d88a751b
satoshi815 <57768913+satoshi815@users.noreply.github.com>
2020-03-11 19:44:51 +0900

    修正しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(text: comment_params[:text], item_id: comment_params[:item_id], user_id: current_user.id)
-    redirect_to "/items/#{comment.item.id}"   #コメントと結びつくアイテムの詳細画面に遷移す
+    redirect_to "item_path(comment.item.id)"   #コメントと結びつくアイテムの詳細画面に遷移す
     if comment.save
       flash[:notice] = "コメントしました"
     else
