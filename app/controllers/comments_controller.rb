91d54444157b3d88faef86a0e48c81bd087fe90d
satoshi815 <57768913+satoshi815@users.noreply.github.com>
2020-03-11 20:50:57 +0900

    エラーハンドリングしました。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.new(text: comment_params[:text], item_id: comment_params[:item_id], user_id: current_user.id)
-    redirect_to "item_path(comment.item.id)"   #コメントと結びつくアイテムの詳細画面に遷移す
     if comment.save
       flash[:notice] = "コメントしました"
+      redirect_to "item_path(comment.item.id)"   #コメントと結びつくアイテムの詳細画面に遷移す
     else
       flash[:notice] = "コメントできませんでした"
+      redirect_to "item_path(comment.item.id)"   #コメントと結びつくアイテムの詳細画面に遷移す
     end
   end
 
