3b9bef814d13ead367d7ff1f7010bb032c569fcd
satoshi815 <57768913+satoshi815@users.noreply.github.com>
2020-03-12 17:48:00 +0900

    修正しました

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
-belongs_to :user
-belongs_to :category
-belongs_to :brand
-has_many :comments #commentsテーブルとのアソシエーション
-has_many :images
-has_one :purchase
+  belongs_to :user
+  belongs_to :category
+  belongs_to :brand
+  has_many :comments #commentsテーブルとのアソシエーション
+  has_many :images
+  has_one :purchase
 end
