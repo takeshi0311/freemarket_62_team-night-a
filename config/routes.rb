89d9a712fced2265b63ed9d3d47e2915455cf9a8
satoshi815 <57768913+satoshi815@users.noreply.github.com>
2020-03-12 14:10:31 +0900

    変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
     resources :items do
-    resources :comments, only: [:create]
+      resources :comments, only: [:create]
   end
   resources :users, only: [:show] do
     collection do
