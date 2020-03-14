735bc28059a80936be144bc5800fde67b75c7d31
satoshi815 <57768913+satoshi815@users.noreply.github.com>
2020-03-14 15:18:33 +0900

    仮コミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,11 @@ class UsersController < ApplicationController
 
   def logout
   end
+
+  def show
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @item = user.item.page(params[:page]).per(5).order("created_at DESC")
+  end
   
 end
