f44394a12c1aa0d3589499230131d848ce328d05
satoshi815 <57768913+satoshi815@users.noreply.github.com>
2020-03-18 16:22:06 +0900

    エラーハンドリング

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,24 @@ class FavoritesController < ApplicationController
 
   def create
     @Favorite = current_user.favorite.create(item_id: params[:item_id])
-    redirect_back(fallback_location: root_path)
+    if @Favorite.save!
+      flash[:notice] = "お気に入りに追加しました"
+      redirect_back(fallback_location: root_path)
+    else 
+      flash[:notice] = "お気に入りに追加できませんでした"
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
     @Favorite = Favorite.find_by(item_id: params[:item_id], user_id: current_user.id)
-    @Favorite.destroy
-    redirect_back(fallback_location: root_path)
+    if @Favorite.destroy!
+      flash[:notice] = "お気に入りを解除しました"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:notice] = "お気に入りを解除できませんでした"
+      redirect_back(fallback_location: root_path)
+    end
   end
 
 end
